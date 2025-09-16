4663a2f3eb7709eafcf719cf20be92b39be7c472
changbin <changbin@DESKTOP-MTHI0U6>
2025-09-17 01:49:25 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Win_init
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkB1 = new System.Windows.Forms.CheckBox();
+            this.checkB2 = new System.Windows.Forms.CheckBox();
+            this.checkB3 = new System.Windows.Forms.CheckBox();
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.panelPage1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnFinish = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.radioSkip = new System.Windows.Forms.RadioButton();
+            this.radioA3 = new System.Windows.Forms.RadioButton();
+            this.radioA2 = new System.Windows.Forms.RadioButton();
+            this.radioA1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelPage2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panelPage1.SuspendLayout();
+            this.panelPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkB1
+            // 
+            this.checkB1.AutoSize = true;
+            this.checkB1.CheckAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.checkB1.Location = new System.Drawing.Point(99, 115);
+            this.checkB1.Name = "checkB1";
+            this.checkB1.Size = new System.Drawing.Size(288, 40);
+            this.checkB1.TabIndex = 0;
+            this.checkB1.Text = "开源软件组合\r\n\r\n包含 FireFox ESR、7-Zip、VLC Media Player 等";
+            this.checkB1.UseVisualStyleBackColor = true;
+            // 
+            // checkB2
+            // 
+            this.checkB2.AutoSize = true;
+            this.checkB2.CheckAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.checkB2.Location = new System.Drawing.Point(99, 182);
+            this.checkB2.Name = "checkB2";
+            this.checkB2.Size = new System.Drawing.Size(234, 40);
+            this.checkB2.TabIndex = 1;
+            this.checkB2.Text = "常用国际化软件组合\r\n\r\n包含 Chrome、Bandizip、Potplayer 等";
+            this.checkB2.UseVisualStyleBackColor = true;
+            // 
+            // checkB3
+            // 
+            this.checkB3.AutoSize = true;
+            this.checkB3.CheckAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.checkB3.Location = new System.Drawing.Point(99, 249);
+            this.checkB3.Name = "checkB3";
+            this.checkB3.Size = new System.Drawing.Size(276, 40);
+            this.checkB3.TabIndex = 2;
+            this.checkB3.Text = "国内办公与通讯软件组合\r\n\r\n包含 QQ、微信、腾讯会议、MindMaster脑图 等\r\n";
+            this.checkB3.UseVisualStyleBackColor = true;
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPrev.Location = new System.Drawing.Point(602, 383);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(75, 23);
+            this.btnPrev.TabIndex = 3;
+            this.btnPrev.Text = "上一步";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            // 
+            // panelPage1
+            // 
+            this.panelPage1.AutoSize = true;
+            this.panelPage1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelPage1.ColumnCount = 3;
+            this.panelPage1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12F));
+            this.panelPage1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73F));
+            this.panelPage1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.panelPage1.Controls.Add(this.label1, 1, 1);
+            this.panelPage1.Controls.Add(this.radioA1, 1, 2);
+            this.panelPage1.Controls.Add(this.radioA2, 1, 3);
+            this.panelPage1.Controls.Add(this.radioA3, 1, 4);
+            this.panelPage1.Controls.Add(this.radioSkip, 1, 5);
+            this.panelPage1.Controls.Add(this.btnNext, 2, 6);
+            this.panelPage1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelPage1.Location = new System.Drawing.Point(0, 0);
+            this.panelPage1.Margin = new System.Windows.Forms.Padding(0);
+            this.panelPage1.Name = "panelPage1";
+            this.panelPage1.RowCount = 7;
+            this.panelPage1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.panelPage1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.panelPage1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.panelPage1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.panelPage1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.panelPage1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.panelPage1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.panelPage1.Size = new System.Drawing.Size(800, 450);
+            this.panelPage1.TabIndex = 5;
+            // 
+            // btnFinish
+            // 
+            this.btnFinish.Location = new System.Drawing.Point(683, 383);
+            this.btnFinish.Name = "btnFinish";
+            this.btnFinish.Size = new System.Drawing.Size(75, 23);
+            this.btnFinish.TabIndex = 4;
+            this.btnFinish.Text = "完成配置";
+            this.btnFinish.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(683, 383);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 4;
+            this.btnNext.Text = "下一步";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // radioSkip
+            // 
+            this.radioSkip.AutoSize = true;
+            this.radioSkip.Location = new System.Drawing.Point(99, 316);
+            this.radioSkip.Name = "radioSkip";
+            this.radioSkip.Size = new System.Drawing.Size(191, 16);
+            this.radioSkip.TabIndex = 3;
+            this.radioSkip.TabStop = true;
+            this.radioSkip.Text = "我不需要安装Microsoft Office";
+            this.radioSkip.UseVisualStyleBackColor = true;
+            // 
+            // radioA3
+            // 
+            this.radioA3.AutoSize = true;
+            this.radioA3.Location = new System.Drawing.Point(99, 249);
+            this.radioA3.Name = "radioA3";
+            this.radioA3.Size = new System.Drawing.Size(299, 16);
+            this.radioA3.TabIndex = 2;
+            this.radioA3.TabStop = true;
+            this.radioA3.Text = "Microsoft Office Pro Plus 2021 Enterprise LTSC\r\n";
+            this.radioA3.UseVisualStyleBackColor = true;
+            // 
+            // radioA2
+            // 
+            this.radioA2.AutoSize = true;
+            this.radioA2.Location = new System.Drawing.Point(99, 182);
+            this.radioA2.Name = "radioA2";
+            this.radioA2.Size = new System.Drawing.Size(299, 16);
+            this.radioA2.TabIndex = 1;
+            this.radioA2.TabStop = true;
+            this.radioA2.Text = "Microsoft Office Pro Plus 2019 Enterprise LTSC\r\n";
+            this.radioA2.UseVisualStyleBackColor = true;
+            // 
+            // radioA1
+            // 
+            this.radioA1.AutoSize = true;
+            this.radioA1.Location = new System.Drawing.Point(99, 115);
+            this.radioA1.Name = "radioA1";
+            this.radioA1.Size = new System.Drawing.Size(299, 16);
+            this.radioA1.TabIndex = 0;
+            this.radioA1.TabStop = true;
+            this.radioA1.Text = "Microsoft Office Pro Plus 2016 Enterprise LTSC";
+            this.radioA1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "请选择您想要安装的Microsoft Office版本：";
+            // 
+            // panelPage2
+            // 
+            this.panelPage2.AutoSize = true;
+            this.panelPage2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panelPage2.ColumnCount = 3;
+            this.panelPage2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12F));
+            this.panelPage2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 73F));
+            this.panelPage2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.panelPage2.Controls.Add(this.label2, 1, 1);
+            this.panelPage2.Controls.Add(this.checkB1, 1, 2);
+            this.panelPage2.Controls.Add(this.checkB2, 1, 3);
+            this.panelPage2.Controls.Add(this.checkB3, 1, 4);
+            this.panelPage2.Controls.Add(this.btnPrev, 1, 6);
+            this.panelPage2.Controls.Add(this.btnFinish, 2, 6);
+            this.panelPage2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelPage2.Location = new System.Drawing.Point(0, 0);
+            this.panelPage2.Margin = new System.Windows.Forms.Padding(0);
+            this.panelPage2.Name = "panelPage2";
+            this.panelPage2.RowCount = 7;
+            this.panelPage2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.panelPage2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.panelPage2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.panelPage2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.panelPage2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.panelPage2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.panelPage2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.panelPage2.Size = new System.Drawing.Size(800, 450);
+            this.panelPage2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(99, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "请选择您需要的其他软件组合";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelPage2);
+            this.Controls.Add(this.panelPage1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panelPage1.ResumeLayout(false);
+            this.panelPage1.PerformLayout();
+            this.panelPage2.ResumeLayout(false);
+            this.panelPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox checkB3;
+        private System.Windows.Forms.CheckBox checkB2;
+        private System.Windows.Forms.CheckBox checkB1;
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.TableLayoutPanel panelPage1;
+        private System.Windows.Forms.Button btnFinish;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.RadioButton radioSkip;
+        private System.Windows.Forms.RadioButton radioA3;
+        private System.Windows.Forms.RadioButton radioA2;
+        private System.Windows.Forms.RadioButton radioA1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel panelPage2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace Win_init
+{
+    public partial class Form1 : Form
+    {
+        // 保存A、B选择
+        private int selectedA = -1; // 0:A1, 1:A2, 2:A3, 3:跳过
+        private List<int> selectedBs = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 事件绑定
+            radioA1.CheckedChanged += RadioA_CheckedChanged;
+            radioA2.CheckedChanged += RadioA_CheckedChanged;
+            radioA3.CheckedChanged += RadioA_CheckedChanged;
+            radioSkip.CheckedChanged += RadioA_CheckedChanged;
+
+            btnNext.Click += BtnNext_Click;
+            btnPrev.Click += BtnPrev_Click;
+            btnFinish.Click += BtnFinish_Click;
+
+            btnNext.Enabled = false;
+            panelPage2.Visible = false;
+            panelPage1.Visible = true;
+        }
+
+        private void RadioA_CheckedChanged(object sender, EventArgs e)
+        {
+            btnNext.Enabled = radioA1.Checked || radioA2.Checked || radioA3.Checked || radioSkip.Checked;
+        }
+
+        private void BtnNext_Click(object sender, EventArgs e)
+        {
+            // 保存A选择
+            if (radioA1.Checked) selectedA = 0;
+            else if (radioA2.Checked) selectedA = 1;
+            else if (radioA3.Checked) selectedA = 2;
+            else if (radioSkip.Checked) selectedA = 3;
+
+            panelPage1.Visible = false;
+            panelPage2.Visible = true;
+        }
+
+        private void BtnPrev_Click(object sender, EventArgs e)
+        {
+            panelPage2.Visible = false;
+            panelPage1.Visible = true;
+        }
+
+        private void BtnFinish_Click(object sender, EventArgs e)
+        {
+            selectedBs.Clear();
+            if (checkB1.Checked) selectedBs.Add(0);
+            if (checkB2.Checked) selectedBs.Add(1);
+            if (checkB3.Checked) selectedBs.Add(2);
+
+            string baseDir = Application.StartupPath;
+            try
+            {
+                // 运行A
+                switch (selectedA)
+                {
+                    case 0: Process.Start(Path.Combine(baseDir, "A1.exe")); break;
+                    case 1: Process.Start(Path.Combine(baseDir, "A2.exe")); break;
+                    case 2: Process.Start(Path.Combine(baseDir, "A3.exe")); break;
+                    case 3: /* 跳过 */ break;
+                }
+                // 运行B
+                foreach (var b in selectedBs)
+                {
+                    Process.Start(Path.Combine(baseDir, $"B{b + 1}.exe"));
+                }
+                MessageBox.Show("配置完成！");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("运行程序时出错: " + ex.Message);
+            }
+        }
+    }
+}
